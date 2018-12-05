325733e5dbb76d6fd5a60bda94b3967c68f1de57
Brendan Bycroft <brendan.bycroft@gmail.com>
2018-12-05 22:27:03 +1300

    Who needs explicit types?

--- Day5.fs
+++ Day5.fs
@@ -4,12 +4,12 @@ open System
 open System.IO
 open System.Collections.Generic
 
-let samePolymer (a: char) (b: char) = Char.Equals(Char.ToLower a, Char.ToLower b)
-let polymersWillReact (a: char) (b: char) = samePolymer a b && not (Char.Equals(a, b))
+let samePolymer a b = Char.Equals(Char.ToLower a, Char.ToLower b)
+let polymersWillReact a b = samePolymer a b && not (Char.Equals(a, b))
 
 let isNotNull a = match a with | null -> false | _ -> true
 
-let calcNumLeftInReaction (text: string) =
+let calcNumLeftInReaction text =
     // sometimes mutable algorithms are just easier, OK!?
     let ll = new LinkedList<char>(text)
     let mutable item = ll.First
@@ -23,7 +23,7 @@ let calcNumLeftInReaction (text: string) =
             item <- item.Next
     ll.Count
 
-let trialRemovingPolymer (text: string) (c: char) =
+let trialRemovingPolymer text c =
     let shorterText = text |> Seq.filter (fun a -> not (samePolymer a c)) |> String.Concat
     calcNumLeftInReaction shorterText
 
